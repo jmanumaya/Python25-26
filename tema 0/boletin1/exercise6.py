bad18bd33a49babef415d52b90fc08e1bda19e4c
Jose Manuel <jm.maya@iesnervion.es>
2025-09-24 11:01:34 +0200

    update

---
+++ tema 0/boletin1/exercise6.py
@@ -0,0 +1,7 @@
+# Pedir un número y calcular su factorial. 
+# Por ejemplo, el factorial de 5 se denota 5! 
+# y es igual a 5x4x3x2x1 = 120.
+
+# Promp the user a number
+number = int(input("Please enter a number: "))
+
---
+++ tema 0/boletin1/exercise7.py
@@ -0,0 +1,1 @@
+# Realiza un programa que pida un número entero positivo y nos diga si es primo o no.
