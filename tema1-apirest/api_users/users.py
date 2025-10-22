4e7e0f89d00032acba248a198d9494f9058f649b
Jose Manuel <jm.maya@iesnervion.es>
2025-10-22 09:27:33 +0200

    update

--- tema1-apirest/api_users/users.py
+++ tema1-apirest/api_users/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -15,23 +15,44 @@ users_list = [
     User(id=3, name="JoseMa", surname="Maya", age=20)
 ]
 
-
 @app.get("/users")
 def get_users():
     """Devuelve toda la lista de usuarios"""
     return users_list
-
 
 @app.get("/users/{id_user}")
 def get_user_by_id(id_user: int):
     """Devuelve un usuario por su ID desde la ruta"""
     return search_user(id_user)
 
-
-@app.post("/users/")
+@app.get("/users/")
 def get_user(id: int):
     return search_user(id)
 
+@app.post("/users", status_code=201)
+def add_user(user: User):
+    user.id = next_id()
+    users_list.append(user)
+    return user
+
+@app.put("/users/{id}", response_model=User)
+def modify_user(id: int, user: User):
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            user.id = id
+            users_list[index] = user
+            return user
+    
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.delete("/users/{id}")
+def delete_user(id:int):
+    for saved_user in users_list:
+        if saved_user.id == id:
+            users_list.remove(saved_user)
+            return {}
+    raise HTTPException(status_code=404, detail="User not found")
+    
 
 def search_user(id: int):
     """Función auxiliar para buscar un usuario por su ID"""
@@ -39,3 +60,6 @@ def search_user(id: int):
     if users:
         return users[0]
     return {"error": "user not found"}
+
+def next_id():
+    return (max(users_list, key=id).id + 1)
