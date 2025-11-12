c3ee0207c35bb29b4f1ebbf815a84cff6065b495
Jose Manuel <jm.maya@iesnervion.es>
2025-11-12 09:37:43 +0100

    update fururla

--- tema1-apirest/ejercicio1/ejercicio9/router/auth_users.py
+++ tema1-apirest/ejercicio1/ejercicio9/router/auth_users.py
@@ -1,5 +1,5 @@
 
-import datetime
+from  datetime import datetime, timedelta, timezone
 from pydantic import BaseModel
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -57,21 +57,43 @@ def register(user: UserDB):
     if user.username not in users_db:
         hashed_password = password_hash.hash(user.password)
         user.password = hashed_password
-        users_db[user.username] = user
+        users_db[user.username] = user.model_dump()
         return user
     else:
         raise HTTPException(status_code=409, detail="User already exists")
     
 @router.post("/login")
 async def login (form: OAuth2PasswordRequestForm = Depends()):
-    user = users_db.get(form.username)
-    if user:
+    user_db = users_db.get(form.username)
+    if user_db:
         # Si el usuario existe en la base de datos
         # Comprobamos las contraseñas
-        if password_hash.verify(user.password, user["password"]):
-            expire = datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-            access_token = {"sub" : user.username, "exp" : expire}
-            # Generamos token
-            token = jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM)
-            return {"acces_token":token, "token_type":"bearer"}
+        # Creamos el usuario de tipo UserDB
+        user = UserDB(**users_db[form.username])
+        try:
+            if password_hash.verify(form.password, user.password):
+                expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+                access_token = {"sub" : user.username, "exp" : expire}
+                # Generamos token
+                token = jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM)
+                return {"access_token": token, "token_type": "bearer"}
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"Error en la autenticación: {e}")
     raise HTTPException(status_code=401, detail="Usuario o contraseña icorrectos")
+
+# @router.post("/login")
+# async def login(form: OAuth2PasswordRequestForm = Depends()):
+#     user = users_db.get(form.username)
+#     if not user:
+#         raise HTTPException(status_code=401, detail="Usuario o contraseña incorrectos")
+
+#     # Verificar contraseña
+#     if not password_hash.verify(form.password, user["password"]):
+#         raise HTTPException(status_code=401, detail="Contraseña incorrecta")
+
+#     # Crear token
+#     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#     access_token = {"sub": user["username"], "exp": expire}
+#     token = jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM)
+
+#     return {"access_token": token, "token_type": "bearer"}
