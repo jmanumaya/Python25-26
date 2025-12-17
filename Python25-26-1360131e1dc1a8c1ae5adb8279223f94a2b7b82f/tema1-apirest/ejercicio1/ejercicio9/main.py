aa2092e097e3cfeb94f00d352e09449a8f7cecfb
Jose Manuel <jm.maya@iesnervion.es>
2025-12-17 09:02:49 +0100

    update

--- Python25-26-1360131e1dc1a8c1ae5adb8279223f94a2b7b82f/tema1-apirest/ejercicio1/ejercicio9/main.py
+++ Python25-26-1360131e1dc1a8c1ae5adb8279223f94a2b7b82f/tema1-apirest/ejercicio1/ejercicio9/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
 from router import journalist, article, auth_users, journalist_db, article_db
+import os
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Routers
 app.include_router(journalist_db.router)
 app.include_router(journalist.router)
 app.include_router(article_db.router)
 app.include_router(article.router)
 app.include_router(auth_users.router)
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")   
+if os.path.isdir("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
 def inicio():
     return {"message": "Welcome to the News API"}
